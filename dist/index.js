bda047247d7984a63a912cfb5d31008f3c0bca07
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-04-26 13:29:59 +0200

    Create index.js

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+try {
+    var portHTTP = process.env.HTTP_PORT;
+}
+catch (error) {
+}
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFJQSxrREFBNEI7QUFFNUIsZ0JBQU0sQ0FBQyxNQUFNLEVBQUUsQ0FBQztBQUVoQixJQUFJO0lBRUEsSUFBTSxRQUFRLEdBQVcsT0FBTyxDQUFDLEdBQUcsQ0FBQyxTQUFTLENBQUM7Q0FFbEQ7QUFBQyxPQUFNLEtBQUssRUFBRTtDQUVkIn0=
