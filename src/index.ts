beff33d7deb7ad13763f76d2d7effa0998f5f46e
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-04-26 12:42:45 +0200

    type installs

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import fs from 'fs';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
+try {
+    
+    const portHTTP = <string>process.env.HTTP_PORT;
+    
+} catch(error) {
+    
+}
